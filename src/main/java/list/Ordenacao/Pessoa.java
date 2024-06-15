f129263301bb19989e4b4f380a9abf96b51f1c94
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2024-06-15 06:20:15 -0300

    Ordenacao adicionada

---
+++ src/main/java/list/Ordenacao/Pessoa.java
@@ -0,0 +1,43 @@
+package main.java.list.Ordenacao;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + ", Idade: " + idade + ", Altura: " + altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(this.idade, p.getIdade());
+    }
+}
+
+class ComparatorPorAltura implements Comparator<Pessoa> {
+    @Override
+    public int compare(Pessoa p1, Pessoa p2){
+        return Double.compare(p1.getAltura(), p2.getAltura());
+    }
+}
